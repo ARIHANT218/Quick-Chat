0ec15346801f2df2eaa22cec74583e86f2768211
ARIHANT <157610621+ARIHANT218@users.noreply.github.com>
2025-05-18 13:13:43 +0530

    Initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,11 @@
 // api.js
 import axios from 'axios';
+export const api = axios.create({
+  baseURL: import.meta.env.MODE === "development"
+    ? 'http://localhost:5000/api'
+    : import.meta.env.VITE_API_URL, // Use environment variable in prod
 
-export const api =  axios.create({
-
-  baseURL: import.meta.env.MODE === "development"? 'http://localhost:5000/api' : "/api",
-
-  withCredentials: true, 
+  withCredentials: true,
 });
-
 
 
