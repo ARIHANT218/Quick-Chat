344abc893bf6479974a069a3e37373173490ac3c
ARIHANT <157610621+ARIHANT218@users.noreply.github.com>
2025-05-19 19:49:39 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://quick-chat-1-wblr.onrender.com/api" : "/api",
+  baseURL:"https://quick-chat-1-wblr.onrender.com/api",
   withCredentials: true,
 });
 
